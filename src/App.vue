9b43c275bad4a0957b7acd51b5d1da1dc8546738
Extomvi <exceltadedokun@gmail.com>
2020-06-23 09:20:26 +0100

    Updated App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <v-app>
-  <v-navigation-drawer v-model="drawer" app>
-    <!-- -->
-  </v-navigation-drawer>
- 
-  <v-app-bar app>
-    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-  </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app>
+      <!-- -->
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
-  <v-main>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-main>
-
-  <v-footer app>
-    <!-- -->
-  </v-footer>
-</v-app>
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
