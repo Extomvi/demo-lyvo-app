e0f3101fffe754b569df20d7233597de77fbd305
Extomvi <exceltadedokun@gmail.com>
2020-06-24 16:11:57 +0100

    Updated App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" app>
-      <!-- -->
+      <v-list nav dense>
+        <v-list-item-group active-class="primary--text">
+          <v-list-item @click="routeTo('/')">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
+
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
