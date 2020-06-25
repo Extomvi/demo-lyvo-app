98833bc1a485fa679ba639a71f33de79cc0776c7
Extomvi <exceltadedokun@gmail.com>
2020-06-25 20:45:27 +0100

    Updated Appform.vue

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -2,10 +2,7 @@
   <v-form v-model="valid">
     <v-container>
       <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
+        <v-col cols="12" md="4">
           <v-text-field
             v-model="firstname"
             :rules="nameRules"
@@ -15,10 +12,7 @@
           ></v-text-field>
         </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
+        <v-col cols="12" md="4">
           <v-text-field
             v-model="lastname"
             :rules="nameRules"
@@ -28,10 +22,7 @@
           ></v-text-field>
         </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
+        <v-col cols="12" md="4">
           <v-text-field
             v-model="email"
             :rules="emailRules"
@@ -40,10 +31,28 @@
           ></v-text-field>
         </v-col>
       </v-row>
+      <v-row>
+        <v-btn color="primary">Submit</v-btn>
+      </v-row>
     </v-container>
   </v-form>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+    }),
+}
 </script>
