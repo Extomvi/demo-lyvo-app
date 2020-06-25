10ae323244129303e79f2e64a446873f680477cf
Extomvi <exceltadedokun@gmail.com>
2020-06-25 22:02:05 +0100

    Updated index.js file

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const SUBMIT_REQUEST = 'SUBMIT_REQUEST'
+const SUBMIT_SUCCESS = 'SUBMIT_SUCCESS'
+const SUBMIT_FAILURE = 'SUBMIT_FAILURE'
+
 
 export default new Vuex.Store({
     state: {
+        loading: false,
         alertMsg: '',
     },
     mutations: {
-        success(state) {
-            state.alertMsg = 'Success'
+        [SUBMIT_REQUEST](state) {
+            state.loading = true
+        },
+        [SUBMIT_SUCCESS](state) {
+            state.alertMsg = "Submission Successful"
+            state.loading = false
+        },
+        [SUBMIT_FAILURE](state) {
+            state.alertMsg = 'Submission failed'
+            state.loading = false
         }
     },
     actions: {},
