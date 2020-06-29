c703b805fbaf3df066e4de26c5d7f55e1738a021
Extomvi <exceltadedokun@gmail.com>
2020-06-29 23:12:06 +0100

    Updated index.js file

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable prettier/prettier */
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
@@ -18,7 +17,7 @@ const routes = [{
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/About.vue'),
+            import ('../views/About.vue'),
     },
     {
         path: '/application',
